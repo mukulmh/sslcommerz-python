2038e7a7d1f2a87d93cafb6d7ee9b70ab4c66d97
mukulmh <mukulseu@gmail.com>
2023-02-18 16:13:59 +0600

    issue fixed

--- app_payment/views.py
+++ app_payment/views.py
@@ -1,40 +1,51 @@
 from django.shortcuts import render, redirect
 from sslcommerz_python.payment import SSLCSession
-from decimal import Decimal
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
-import secrets, string
+import secrets
+import string
 from .models import Transactions
 from django.core.serializers import serialize
 
+
 @csrf_exempt
 def Card(request):
-    objs = serialize('json', Transactions.objects.filter(status = 'SUCCESS'))
-    print(objs)
+    # objs = serialize('json', Transactions.objects.filter(status='SUCCESS'))
+    # print(objs)
     return render(request, 'card.html')
 
+
 def Checkout(request):
-    transaction_id = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for i in range(18))
+    transaction_id = ''.join(secrets.choice(
+        string.ascii_uppercase + string.digits) for i in range(18))
     amount = 100
 
-    mypayment = SSLCSession(sslc_is_sandbox=True, sslc_store_id='devel634786da0d226', sslc_store_pass='devel634786da0d226@ssl')
+    mypayment = SSLCSession(sslc_is_sandbox=True, sslc_store_id='devel634786da0d226',
+                            sslc_store_pass='devel634786da0d226@ssl')
 
-    mypayment.set_urls(success_url='http://127.0.0.1:8000/status', fail_url='http://127.0.0.1:8000/status', cancel_url='http://127.0.0.1:8000/status', ipn_url='http://127.0.0.1:8000/status')
+    mypayment.set_urls(success_url='http://127.0.0.1:8000/status', fail_url='http://127.0.0.1:8000/status',
+                       cancel_url='http://127.0.0.1:8000/status', ipn_url='http://127.0.0.1:8000/status')
 
-    mypayment.set_product_integration(tran_id=transaction_id, total_amount=amount, currency='BDT', product_category='clothing', product_name='demo-product', num_of_item=2, shipping_method='YES', product_profile='None')
+    mypayment.set_product_integration(tran_id=transaction_id, total_amount=amount, currency='BDT', product_category='clothing',
+                                      product_name='demo-product', num_of_item=2, shipping_method='YES', product_profile='None')
 
-    mypayment.set_customer_info(name='John Doe', email='johndoe@email.com', address1='demo address', address2='demo address 2', city='Dhaka', postcode='1207', country='Bangladesh', phone='01711111111')
+    mypayment.set_customer_info(name='John Doe', email='johndoe@email.com', address1='demo address',
+                                address2='demo address 2', city='Dhaka', postcode='1207', country='Bangladesh', phone='01711111111')
 
-    mypayment.set_shipping_info(shipping_to='demo customer', address='demo address', city='Dhaka', postcode='1209', country='Bangladesh')
+    mypayment.set_shipping_info(shipping_to='demo customer', address='demo address',
+                                city='Dhaka', postcode='1209', country='Bangladesh')
 
     # If you want to post some additional values
-    mypayment.set_additional_values(value_a='cusotmer@email.com', value_b='portalcustomerid', value_c='1234', value_d='uuid')
+    mypayment.set_additional_values(
+        value_a='cusotmer@email.com', value_b='portalcustomerid', value_c='1234', value_d='uuid')
 
     response_data = mypayment.init_payment()
     print(response_data)
-    if (response_data['status']== 'SUCCESS'):
-        Transactions.objects.create(tran_id = transaction_id, amount = amount, status='PENDING')
+    if (response_data['status'] == 'SUCCESS'):
+        Transactions.objects.create(
+            tran_id=transaction_id, amount=amount, status='PENDING')
         return redirect(response_data['GatewayPageURL'])
+
 
 @csrf_exempt
 def Status(request):
@@ -61,7 +72,7 @@ def Status(request):
         pay_obj.bank_tran_id = request_obj['bank_tran_id']
         pay_obj.save()
         messages.error(request, 'Your payment has Failed!')
-    
+
     if request_obj['status'] == 'VALID':
         pay_obj.status = 'SUCCESS'
         pay_obj.card_type = request_obj['card_type']
